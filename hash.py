107c709f15a314de94837cd0ad01083190faf88e
Roy Telles <rtelles64@gmail.com>
2020-02-20 10:52:09 -0800

    Add read input file function

--- hash.py
+++ hash.py
@@ -1,10 +1,10 @@
 # INPUT
 
 # 6 2 7 - 6 books, 2 libraries, 7 days of scanning
-# 1 2 3 6 5 4 - scores of the books
+# 1 2 3 6 5 4 - scores of the books (in order)
 # 5 2 2 - Lib0 has 5 books, signup takes 2 days, can ship 2 books per day
 # 0 1 2 3 4 - books in Lib0
-# 4 3 1 - Lib1 has 4 books, singup takes 3 days, can ship 1 book per day
+# 4 3 1 - Lib1 has 4 books, signup takes 3 days, can ship 1 book per day
 # 0 2 3 5 - books in Lib1
 
 # OUTPUT
@@ -14,3 +14,17 @@
 # 5 2 3 - Lib1 will send book 5, book 2, book 3 in order
 # 0 5 - Second library to sign up is Lib0, after signup, send 5 books
 # 0 1 2 3 4 - Lib0 will send book 0, 1, 2, 3, 4 in order
+
+# Get File info
+def get_input(file_name):
+    capacity = None
+    pizza_type = None
+    with open(file_name) as descriptor:
+        print("reading file")
+        line = descriptor.readline().split(" ")
+        capacity = int(line[0])
+        pizza_types = descriptor.readline().split(" ")
+        print("got types")
+        pizza_types = [int(item) for item in pizza_types]
+        print("converted types")
+    return capacity, pizza_types
