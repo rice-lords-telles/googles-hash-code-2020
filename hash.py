089f856a65991297338bb96b8f41be3603d9694b
Roy Telles <rtelles64@gmail.com>
2020-02-20 10:23:47 -0800

    Preliminary coding file

---
+++ hash.py
@@ -0,0 +1,6 @@
+# 6 2 7 - 6 books, 2 libraries, 7 days of scanning
+# 1 2 3 6 5 4 - scores of the books
+# 5 2 2 - Lib0 has 5 books, signup takes 2 days, can ship 2 books per day
+# 0 1 2 3 4 - books in Lib0
+# 4 3 1 - Lib1 has 4 books, singup takes 3 days, can ship 1 book per day
+# 0 2 3 5 - books in Lib1
