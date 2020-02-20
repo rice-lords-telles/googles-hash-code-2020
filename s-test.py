9b4563d7d7c5df052b7c6f92a950af9a8ba0a866
Steven Lord <lords@oregonstate.edu>
2020-02-20 10:30:49 -0800

    boiler plate for read in data

--- s-test.py
+++ s-test.py
@@ -2,7 +2,29 @@
 
 
 def main():
-    pass
+    line_num = 1
+    library_line_one = True
+
+    with open("a_example.txt") as data:
+        line = data.read()
+        if line_num == 1:
+            # First line of file = [num of diff books][num of libraries][days for scanning]
+            pass
+        elif line_num == 2:
+            # Second line of file = [score of each book]
+            pass
+        else:
+            # iterating through libraries
+            if library_line_one:
+                #first line of library
+                library_line_one != library_line_one
+            else:
+                #second line of library
+                library_line_one != library_line_one
+            
+            
+        line_num += 1
+
     
 
 
