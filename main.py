ca3127c12449f342542510b73bffb81ce66e83a6
patrick rice <patrickrice20@gmail.com>
2020-02-20 11:33:26 -0800

    Create test file for testing submissions

--- main.py
+++ main.py
@@ -22,6 +22,34 @@
 # }
 
 # Example
+# {
+#     total_books: 6,
+#     total_libraries: 2,
+#     total_days: 7,
+#     book_scores: [1, 2, 3, 6, 5, 4],
+#     libraries:[
+#         {
+#             books: [0, 1, 2, 3, 4],
+#             signup: 2,
+#             # books per day
+#             bpd: 2
+#         },
+#         {
+#             books: [0, 2, 3, 5],
+#             signup: 3,
+#             bpd: 1
+#         }
+#     ]
+# }
+TEST_INPUTS = [
+    "a_example.txt",
+    "b_read_on.txt",
+    "c_incunbul.txt",
+    "d_tough_choices.txt",
+    "d_tough_choices",
+    "e_so_many+books.txt",
+    "f_libraries_of_the_world.txt",
+]
 
 
 def algorithm_template(file_path):
@@ -37,7 +65,7 @@ def get_input():
         total_libraries: 0,
         total_days: 0,
         books: [7, 3, 6, 9]
-        libraries:[
+        library_id:[
             {
                 books: [2],
                 signup: 0,
@@ -52,6 +80,7 @@ def get_input():
     library_line_one = (
         True  # used to track whether we are on line 1 or line2 of each library
     )
+    library_id = 0  # used to track library ID for assignment
 
     with open("a_example.txt") as data:
         line = data.readline()
@@ -59,14 +88,15 @@ def get_input():
             # First line of file = [num of diff books][num of libraries][days for scanning]
             total_books = line.split(" ", 0)
             total_libraries = line.split(" ", 1)
-            total_days = line.spl(" ", 2)
+            total_days = line.split(" ", 2)
         elif line_num == 2:
             # Second line of file = [score of each book]
-            pass
+            books = [int(i) for i in line.split()]
         else:
             # iterating through libraries
             if library_line_one:
                 # first line of library
+                library_id = {"books": []}
                 library_line_one != library_line_one
             else:
                 # second line of library
--- score_submissions.py
+++ score_submissions.py
@@ -1,14 +1,33 @@
 import pytest
 import sys
+from main import get_input
 
 
-class MinimumScoreThreshold:
-    def setup_class():
+class TestAndScoreAlgorithms(object):
+    def setup_class(self):
+
+        # {function: Function,  score: Number, output: return from Function}
+        self.algorithms = []
+        self.file_path = ""
+        self.input = get_input(self.file_path)
+        print("Algorithm\tScore")
+        for algorithm in self.algorithms:
+            algorithm["output"] = algorithm["function"](self.input)
+            algorithm["score"] = get_score(algorithm["output"])
+            print("{}\t{}".format(algorithm["function"].__name__, algorithm["score"]))
+
+    def test_valid_output(self):
+        for algorithm in self.algorithms:
+            output = algorithm["output"]
+            assert output.length % 2 != 0
+
+    def test_score_(self):
         pass
 
-    def test_score_():
-        pass
+
+def get_score(output):
+    return -1
 
 
 if __name__ == "__main__":
-    sys.exit(pytest.main(["--color=yes", "--verbose", "-s"]))
+    sys.exit(pytest.main([__file__, "--color=yes", "--verbose", "-s"]))
